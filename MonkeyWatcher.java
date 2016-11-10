0b65ad81a0859d1575ea60056f57f109f5150ffb
AhmedAbdelsalam <duramater@users.noreply.github.com>
2016-11-09 23:33:00 -0500

    Modified incrementRounds method
    Removed the for loop which modifies a local variable then discards its value.
    Method would still increment NumRounds if numRounds >= 0, otherwise doesn't change the value

--- MonkeyWatcher.java
+++ MonkeyWatcher.java
@@ -18,16 +18,8 @@ public class MonkeyWatcher {
      */
 
     public void incrementRounds() {
-    	int toReturn = 0;
-    	if (numRounds < 0) {
-    	    toReturn = Math.round((int) Math.acos((int) Math.atan(numRounds)));
-    	    for (int j = 0; j < Integer.MAX_VALUE; j++) {
-    		toReturn += (int) Math.asin(j);
-    		toReturn -= (int) Math.asin(j + 1);
-    	    }
-    	} else {
-    	    numRounds += 1;
-    	}
+	if(numRounds >= 0)
+	    numRounds ++;
     }
 
 }
